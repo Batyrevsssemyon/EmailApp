66f0b65e008a620f1b067be6765f59ed14ec7cd1
Semmma <55916069+Batyrevsssemyon@users.noreply.github.com>
2023-12-14 23:01:20 +0700

    Add files via upload

---
+++ src/test/java/ru/batsss/email/EmailApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.batsss.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
